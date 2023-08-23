a5019aca5ff366bb8e22668e7e4fd1f198b277ae
Linus Ngwee <linusngwee@yahoo.com>
2023-08-24 00:57:41 +0100

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Chat from '../components/Chat'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Bookbuddy',
+  description: 'Your Bookstore for fantasy & mystery novels',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Chat />
+        {children}
+      </body>
+    </html>
+  )
+}
